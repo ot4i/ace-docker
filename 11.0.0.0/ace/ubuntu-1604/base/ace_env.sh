edfbbef32dc17abca497a1055aad88144345e2e5
dan robinson <dan.robinson@uk.ibm.com>
2018-06-04 15:36:05 +0100

    Add copyright statement to Dockerfiles

--- 11.0.0.0/ace/ubuntu-1604/base/ace_env.sh
+++ 11.0.0.0/ace/ubuntu-1604/base/ace_env.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# © Copyright IBM Corporation 2015.
+
+# © Copyright IBM Corporation 2018.
 #
 # All rights reserved. This program and the accompanying materials
-# are made available under the terms of the Eclipse Public License v1.0
+# are made available under the terms of the Eclipse Public License v2.0
 # which accompanies this distribution, and is available at
-# http://www.eclipse.org/legal/epl-v10.html
+# http://www.eclipse.org/legal/epl-v20.html
 
 if [ -z "$MQSI_VERSION" ]; then
   echo "Sourcing profile"
